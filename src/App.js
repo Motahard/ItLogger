f1fcf36e51770be969474ed6b0235f5ddf6a039c
Motahard <mas280499@gmail.com>
2019-10-08 00:47:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -10,7 +19,21 @@ const App = () => {
     M.AutoInit();
   });
 
-  return <div className='App'>My App</div>;
+  return (
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
+  );
 };
 
 export default App;
---
+++ src/components/techs/TechItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { deleteTech } from '../../actions/techActions';
+import PropTypes from 'prop-types';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const TechItem = ({ tech: { firstName, lastName, id }, deleteTech }) => {
+  const onDeleteTech = () => {
+    deleteTech(id);
+    M.toast({ html: `${firstName} ${lastName} technician deleted` });
+  };
+
+  return (
+    <li className='collection-item'>
+      <div>
+        {firstName} {lastName}
+        <a href='#!' className='secondary-content' onClick={onDeleteTech}>
+          <i className='material-icons grey-text'>delete</i>
+        </a>
+      </div>
+    </li>
+  );
+};
+
+TechItem.propTypes = {
+  tech: PropTypes.object.isRequired,
+  deleteTech: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { deleteTech }
+)(TechItem);
